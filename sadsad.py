a2d1a93e5a761faeb7610eaf88b91ef1300e534e
b3nwong <53283480+b3nwong@users.noreply.github.com>
2019-08-27 23:15:01 +0800

    Delete sadsad.py

--- sadsad.py
+++
@@ -1,7 +0,0 @@
-import csv
-with open('places.csv', newline='') as csvfile:
-    travel_excel = csv.reader(csvfile)
-    list_numb=0
-    for row in travel_excel:
-        list_numb+=1
-        print("{}. {} in {} priority {} {}".format(list_numb,row[0],row[1],row[2],row[3]))
